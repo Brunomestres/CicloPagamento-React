5cbd299aac5a3f728eb15a7f2ec6059a2206c7f0
Bruno Ribeiro <brunomestres@gmail.com>
2019-10-18 20:24:15 -0300

    Header Add

---
+++ frontend/src/main/app.jsx
@@ -0,0 +1,9 @@
+import '../common/template/dependencies';
+import React from 'react';
+import Header from '../common/template/Header';
+
+export default props => (
+    <div className="wrapper">
+        <Header />
+    </div>
+)
